16b42a7f61600becfabfa3e605c70a85cd8125b4
Dan Walkes <walkes@colorado.edu>
2026-09-19 13:13:34 +0000

    build.sh: run setup if needed
    Automatically run the setup script if not already run, so the
    build step is the only step needed to build.

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+INIT_ENV_SCRIPT=${SCRIPT_DIR}/bitbake-builds/yocto-hello-world-wrynose-ecen5013/build/init-build-env
+if [ ! -e ${INIT_ENV_SCRIPT} ]; then
+    ${SCRIPT_DIR}/setup.sh
+fi
+
 source "${SCRIPT_DIR}/bitbake-builds/yocto-hello-world-wrynose-ecen5013/build/init-build-env"
 
 bitbake core-image-ecen5013
