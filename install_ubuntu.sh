da75b7a08d7075fe9db2051727584fc8cb253bfc
Dan Walkes <walkes@colorado.edu>
2026-09-19 19:21:21 +0000

    install_ubuntu: remove script
    No longer up to date/supported.

--- install_ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# See Yocto quick build instructions at https://www.yoctoproject.org/docs/2.6/brief-yoctoprojectqs/brief-yoctoprojectqs.html
-pushd `dirname $0`
-sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib \
-     build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
-     xz-utils debianutils iputils-ping libsdl1.2-dev xterm
-sudo apt-get install git
-. ./build_prep.sh
